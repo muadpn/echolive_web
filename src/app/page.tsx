af199e379bd765552d272352fbbd6487d76f7241
neilnicky <neilnicholaskanat@gmail.com>
2025-02-06 07:38:11 +0530

    feat: create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-
+import CodeBlock from "@/components/landingpage/CodeBlock";
+import ComingSoon from "@/components/landingpage/ComingSoon";
+import Features from "@/components/landingpage/Features";
+import Hero from "@/components/landingpage/Hero";
+import Pricing from "@/components/landingpage/Pricing";
+import Privacy from "@/components/landingpage/Privacy";
 import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-
-
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-       
+      <main className="flex min-h-screen flex-col">
+        <div className="flex-grow">
+          <Hero />
+          <CodeBlock />
+          <Features />
+          <Privacy />
+          <ComingSoon />
+          <Pricing />
+        </div>
       </main>
     </HydrateClient>
   );
