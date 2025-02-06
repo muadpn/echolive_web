6919eb6da35d9da6e01e3d45d3921435464105c6
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-02-06 15:49:38 +0530

    feat: Initialised supabase, added Email and github authentication, auth pages, server actions

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,92 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+interface AuthResponse {
+  error: null | string;
+  success: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+  data: unknown | null;
+}
+
+export async function signup(formData: FormData): Promise<AuthResponse> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.signUp({
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+    options: {
+      data: {
+        name: formData.get("name") as string,
+      },
+    },
+  });
+
+  console.log("data:", data);
+
+  if (error) {
+    console.log(error);
+    return {
+      data: null,
+      error: "Signup failed please try again",
+      success: false,
+    };
+  }
+  return {
+    data,
+    error: null,
+    success: true,
+  };
+}
+
+export async function signin(formData: FormData): Promise<AuthResponse> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  });
+
+  return {
+    data,
+    error: error?.message ?? null,
+    success: !error,
+  };
+}
+
+export async function signout(): Promise<void> {
+  const supabase = await createClient();
+
+  await supabase.auth.signOut();
+
+  redirect("/auth/signin");
+}
+
+export async function githubAuth() {
+  /**
+   * NOTE:
+   * - Currently github auth is done via personal github and supabase account
+   * - Whenever using production supabase and domain, you need to check the below mentioned points
+   *      - change the redirect url and main website url in github OAUTH page,
+   *        supabase configuration under authentication section and the env before commit
+   *
+   * */
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: `${process.env.DOMAIN}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    return { error };
+  }
+
+  // console.log(data);
+
+  if (data.url) {
+    redirect(data.url); // use the redirect API for your server framework
+  }
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-
+import { Toaster } from "sonner";
 import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
@@ -17,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          {children}
+          <Toaster richColors expand />
+        </TRPCReactProvider>
       </body>
     </html>
   );
