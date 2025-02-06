aac2c4192824768fd3d9210622c8a08a915abe95
neilnicky <neilnicholaskanat@gmail.com>
2025-02-06 08:30:42 +0530

    fix: remove unused 'SpringConfig' import in SplitText

--- src/components/ui/Splittext.tsx
+++ src/components/ui/Splittext.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useSprings, animated } from "@react-spring/web";
 import type { SpringConfig } from "@react-spring/web";
 import { useEffect, useRef, useState } from "react";
