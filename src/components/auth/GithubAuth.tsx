d6bccb7a22c02b52a05f74c8665328cd7648beff
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-02-08 14:49:42 +0530

    refactor: github auth, auth page redirection

--- src/components/auth/GithubAuth.tsx
+++ src/components/auth/GithubAuth.tsx
@@ -1,31 +1,42 @@
 "use client";
 
-import { Github } from "lucide-react";
+import { Github, Loader2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { githubAuth } from "@/app/actions/auth";
 import { toast } from "sonner";
-import { useId } from "react";
+import { useId, useState } from "react";
 // import { useRouter } from "next/navigation";
 
 export default function GithubAuth() {
+  const [loading, setIsLoading] = useState(false);
   const toastId = useId();
   // const router = useRouter();
 
   async function handleGithubAuth() {
-    toast.loading("Authenticating with github...", { id: toastId });
+    // toast.loading("Authenticating with github...", {
+    //   id: toastId,
+    //   duration: 100,
+    // });
+    setIsLoading(true)
     const res = await githubAuth();
     // console.log("response: ", res);
 
     if (res?.error) {
       // console.log(res.error);
       toast.error("Authentication failed!", { id: toastId });
+      setIsLoading(false)
       return;
     }
 
-    toast.success("Authentication successful!", { id: toastId });
+    // toast.success("Authentication successful!", { id: toastId });
   }
   return (
-    <Button className="w-full bg-green-700 hover:bg-green-800" onClick={handleGithubAuth}>
+    <Button
+      className="w-full bg-green-700 hover:bg-green-800"
+      onClick={handleGithubAuth}
+      disabled={loading}
+    >
+      {loading ? <Loader2 className="animate-spin "/>: ''}
       <Github />
       Sign In with Github
     </Button>
