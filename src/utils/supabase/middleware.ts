42f6a0935cc502d13d6817c439e40b237318b14e
neilnicky <neilnicholaskanat@gmail.com>
2025-02-10 10:52:17 +0530

    chore: updated middleware to allow unauthenticated access to root page

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -28,18 +28,16 @@ export async function updateSession(request: NextRequest) {
   )
 
   // refreshing the auth token
-
   const {
     data: { user },
   } = await supabase.auth.getUser()
   
   if (
     !user &&
-    // !request.nextUrl.pathname.startsWith('/') &&
+    request.nextUrl.pathname !== '/' && // Allow root page access
     !request.nextUrl.pathname.startsWith('/auth') 
-    // !request.nextUrl.pathname.startsWith('/api/webhooks/train')
   ) {
-    // no user, potentially respond by redirecting the user to the login page
+    // no user, redirect to login page
     const url = request.nextUrl.clone()
     url.pathname = '/auth/signin'
     return NextResponse.redirect(url)
