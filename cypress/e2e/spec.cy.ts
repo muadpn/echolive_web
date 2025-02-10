4c6d21af558eaf22cab852ce11676590d2dcb582
neilnicky <neilnicholaskanat@gmail.com>
2025-02-10 12:38:05 +0530

    test: added proper authentication tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,14 @@
-describe("Landing Page", () => {
-  it("Landing Page loads suceffully", () => {
+describe("Initial Page Render", () => {
+  it("should load successfully and display the correct heading", () => {
     cy.visit("http://localhost:3000/");
     cy.get(
       "body > main > div > section.relative.overflow-hidden.px-4.py-20 > div > div > div.container.relative.mx-auto.px-4 > div.mb-6.max-w-md.text-5xl.font-semibold.tracking-tight.md\\:max-w-xl.md\\:text-7xl.lg\\:max-w-4xl > p",
     ).should("have.attr", "aria-label", "Real-time Widgets Made Simple");
   });
+});
 
-  it("Signup", () => {
+describe("Authentication System", () => {
+  it("should allow a user to sign up", () => {
     cy.visit("http://localhost:3000/");
     cy.get('.space-x-4 > [href="/auth/signup"] > .inline-flex').click();
     cy.get('input[id=":r1:-form-item"]').click().type("Jonathan Smith");
@@ -16,7 +18,7 @@ describe("Landing Page", () => {
     cy.get(".space-y-5 > .bg-primary").click();
   });
 
-  it("Login", () => {
+  it("should allow a user to log in", () => {
     cy.visit("http://localhost:3000/");
     cy.get('[href="/auth/signin"] > .inline-flex').click();
     cy.get('input[id=":r1:-form-item"]').type("jon@gmail.com");
@@ -24,7 +26,7 @@ describe("Landing Page", () => {
     cy.get("button[type='submit']").click();
   });
 
-  it("Logout", () => {
+  it("should allow a user to log out", () => {
     cy.visit("http://localhost:3000/auth/signin");
     cy.get(
       ".space-y-5 > :nth-child(1) > .justify-between > .inline-flex",
